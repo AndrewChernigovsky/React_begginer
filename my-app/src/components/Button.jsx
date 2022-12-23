bc454409165e114e93dbd1cb2c86a00f0dc92222
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-12-23 18:11:10 +0300

    2

--- my-app/src/components/Button.jsx
+++ my-app/src/components/Button.jsx
@@ -1,18 +1,43 @@
+import styled from "styled-components/macro";
 
-const input1 = (
-<input type="text" id='input' />
-);
+let blue = blue;
 
-const button1 = (
-<button>push me</button>
-);
+const StyledButton = styled.button`
+	color: black;
+	border: 2px solid black;
+	padding: 2rem;
+	border-radius: 50%;
+	font-size: 40px;
+	transition-duration: 1.7s;
 
+	&:hover {
+		opacity: 0.8;
+		cursor: pointer;
+		color: ${blue};
+		transition-duration: 1.7s;
+	}
 
+`
+
+let array = [
+	0,1,2,3,4,5,6,7,8,9,10
+]
+
+function Button1() {
+	return <StyledButton onClick={() => counter()}>push me</StyledButton>
+};
+function Div1(props) {
+	return <div>{props.count}</div>
+};
+
+function Div2(count) {
+	return <div>{count = 1}</div>
+}
 
 export default function Button() {
 	
 	return <>
-		{button1}
-		{input1}
+		<Button1/>
+		<Div2/>
 	</>
 }
